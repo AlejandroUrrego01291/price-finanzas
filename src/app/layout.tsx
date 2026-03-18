0f8fd443c15ea11b0ea602379b75823832dec0ec
Alejandro Urrego <alejourrego01291@gmail.com>
2026-03-18 12:39:13 -0500

    Update layout metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from './providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Price - Finanzas Personales',
+  title: 'Mis Finanzas Personales',
   description: 'Administra tus ingresos y gastos',
 }
 
